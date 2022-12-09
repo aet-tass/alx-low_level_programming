ed448666482e39a747b5b11612c3f5c75ac24b1e
Ayoub <ayoub.ettass@gmail.com>
2022-12-08 17:18:33 -0800

    done for the last time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/*
- * Description: Prints out a string to stdout.
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int  main(void)
-
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
