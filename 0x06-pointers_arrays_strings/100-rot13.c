63c7b2bbb6cff4e7df809e5472c8d2668c809824
Ayoub <ayoub.ettass@gmail.com>
2022-12-21 16:03:31 -0800

    done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+
+/*
+ * File: 8-rot13.c
+ * Auth: Ayoub Et-tass
+ */
+
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char n[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; z[i] != '\0'; i++)
+	{
+		for (j = 0; j < 56; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = n[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
