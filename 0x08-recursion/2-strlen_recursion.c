1b7f086bdf6f775c20d182c72a9bb3030cbd67fd
Ayoub <ayoub.ettass@gmail.com>
2023-01-04 17:50:21 -0800

    done for the 3rd exo

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: Et-tass Ayoub
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to check
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
