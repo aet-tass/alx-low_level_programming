edb040d0fcf965d14d4f2876e2875d35f7801d04
Ayoub <ayoub.ettass@gmail.com>
2023-01-06 16:57:59 -0800

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-args.c
+ * Auth: Et-tass Ayoub
+ */
+
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+/*
+ * File: 1-args.c
+ * Auth: Et-tass Ayoub
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: result of multiplication or 1
+ **/
+int main(int argc, char **argv)
+{
+	int	a;
+	int	b;
+	int	result;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+/* rthe atoi function convert a string to an int */
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	printf("%d\n", result);
+	return (0);
+}
