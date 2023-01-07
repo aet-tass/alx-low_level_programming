4bc289c0aa530a623a6da369a6f179349c75cf9f
Ayoub <ayoub.ettass@gmail.com>
2023-01-06 16:29:41 -0800

    done for the 1st time

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Et-tass Ayoub
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int ac, char **av)
+{
+	ac = ac;
+	printf("%s\n", av[0]);
+	return (0);
+}
