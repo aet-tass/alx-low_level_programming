3a68ef7b3508654e0b5d58d9109227d2163f3388
Ayoub <ayoub.ettass@gmail.com>
2023-01-22 19:00:08 -0800

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Et-tass Ayoub
+ */
+
+#include"stdlib.h"
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to function that returns nothing
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && *name != '\0' && f != NULL)
+	f(name);
+}
